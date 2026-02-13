a7e8111f3111daabae2e73590746785b5dc12809
Kai <klfn45@gmail.com>
2026-02-13 19:40:04 +0700

    Refactor

--- src/test/java/com/example/individualprep/IndividualprepApplicationTests.java
+++ src/test/java/com/example/individualprep/IndividualprepApplicationTests.java
@@ -1,21 +1,13 @@
 package com.example.individualprep;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import static org.junit.jupiter.api.Assertions.*;
-import com.example.individualprep.service.ArithmeticUtility;
 
 @SpringBootTest
 class IndividualprepApplicationTests {
 
-	@Autowired
-	private ArithmeticUtility arithmeticUtility;
-
 	@Test
-	void testExponent() {
-		double result = arithmeticUtility.exponent(2.0, 3);
-		assertEquals(8.0, result);
+	void contextLoads() {
 	}
 
 }
